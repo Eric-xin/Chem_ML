ce456f7e0481b3b8cd8404c3dee4d4b5358f3f9c
eric-xin <ericxin123@hotmail.com>
2024-11-18 23:41:12 +0000

    minor update - update model structure visualization - adjust dummy input size in graph generator - add the training loss curve for the training on OQMD 500k (full-size) data

--- model/model_graph_generator.py
+++ model/model_graph_generator.py
@@ -1,6 +1,7 @@
 import torch
 from torchviz import make_dot
-from simple_model_residue import Net
+# from simple_model_residue import Net
+from simple_model_revised import Net
 
 import os
 current_dir = os.path.dirname(__file__)
@@ -11,9 +12,8 @@ output_dim = 6
 model = Net(input_dim, output_dim)
 
 # Create a dummy input tensor
-dummy_input = torch.randn(1, input_dim)
+dummy_input = torch.randn(2, input_dim)
 
-# Create the model graph with horizontal layout
 model_graph = make_dot(model(dummy_input), params=dict(model.named_parameters()))
 # model_graph.graph_attr.update(rankdir='LR')
 
